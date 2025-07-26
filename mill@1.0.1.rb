1ba8828a251eafaa4befc12276bb8d98a62c8128
Vitthal Mirji <vitthalmirji@gmail.com>
2025-07-27 00:09:43 +0530

    Update mill@1.0.1.rb

--- mill@1.0.1.rb
+++ mill@1.0.1.rb
@@ -1,11 +1,12 @@
 class MillAT101 < Formula
-  desc "Mill 1.0.1"
+  desc "Mill build tool - v1.0.1"
   homepage "https://com-lihaoyi.github.io/mill"
-  url "https://github.com/com-lihaoyi/mill/releases/download/1.0.1/1.0.1"
+  url "https://repo1.maven.org/maven2/com/lihaoyi/mill-dist/1.0.1/mill-dist-1.0.1-mill.sh"
   version "1.0.1"
-  sha256 "<INSERT_SHA256_HERE>"
+  sha256 "477f81775e2731db2559954a28c5d1e0599b8e94"
 
   def install
-    bin.install "1.0.1" => "mill"
+    bin.install "mill-dist-1.0.1-mill.sh" => "mill"
+    chmod 0755, bin/"mill"
   end
 end
